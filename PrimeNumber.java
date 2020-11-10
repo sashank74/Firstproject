ea3ca34dea648d2c6c65b7cffc7f5c922823cdb1
sashank74 <60648586+sashank74@users.noreply.github.com>
2020-11-10 19:37:32 +0545

    Fixed minor bug

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -3,22 +3,22 @@ public class PrimeNumber{
 	public static void main(String[] args){
 		Scanner Input = new Scanner(System.in);
 		System.out.println("Enter if the number in prime or not");
+
 		long num = Input.nextLong();
 		long count=0;
+
 		for(long i=1; i<=Math.sqrt(num); i++){
 			if(num%i==0){
 				count=count+1;
+			}}
 
-			}
+		if(count==1)
 
-		}
-			if(count==2)
+			System.out.println("prime");
 
-				System.out.println("prime");
+		else
 
-			else
-
-				System.out.println("not prime");
+			System.out.println("not prime");
 
 	}
 }
