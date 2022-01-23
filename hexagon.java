b855b5764ec29099dc9dc2b8d8ffb28647d08aa8
OKNAHSAS <sashankkoirala7@gmail.com>
2022-01-23 09:18:22 +0545

    Added useful input message

--- hexagon.java
+++ hexagon.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class hexagon {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter a number for length of a side of the hexagon");
+        System.out.println("Enter a number to the length of a side of the hexagon");
         double s = in.nextDouble();
         double n = (6 * (s * s)) / (4 * Math.tan(Math.PI / 6));
         System.out.println("The Area of the hexagon is :" + n + "\n");
